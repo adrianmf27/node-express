6a60d1eb8ae2b19e1914e41bc7c4c1e31d66eeb4
Adrián Martínez Fuentes <UO295454@uniovi.es>
2024-02-25 14:23:22 +0100

    New changes

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express")
 
 let app = express()
 
+/* Ejemplo de uso, localhost:8081/imagen1.jpg */
+app.use(express.static("public")) /* Donde primero mira la app por cosas */
+
 app.listen(8081, () => {
     console.log("Server in 8081")
 })
+
